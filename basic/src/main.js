136ccd7c9b7533be3e0708219d8f579d2afd1ca0
rriashok <rriashok@gmail.com>
2026-05-08 13:08:29 +0530

    first push

---
+++ basic/src/main.js
@@ -0,0 +1,44 @@
+import './style.css'
+import javascriptLogo from './assets/javascript.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div class="bg-blue-500 text-white p-10 rounded-xl shadow-xl text-3xl">
+    Hello Tailwind CSS 🚀
+  </div>
+<div class="grid grid-cols-5 items-start gap-10 justify-between">
+    <div class="bg-amber-200 m-2 p-3 flex-5 inline-block">
+      Welcome to AIE India
+    </div>
+
+    <div class="bg-amber-200 m-2 p-3 inline-block">
+      Welcome to AIE Hyd
+    </div>
+    <div class="bg-amber-200 m-2 p-3 inline-block">
+      Welcome to AIE Indiaa
+    </div>
+
+    <div class="bg-amber-200 m-2 p-3 inline-block">
+      Welcome to AIE Hyd
+    </div>
+    <div class="bg-amber-200 m-2 p-3 inline-block">
+      Welcome to AIE India
+    </div>
+
+    <div class="bg-amber-200 m-2 p-3 inline-block">
+      Welcome to AIE Hyd
+    </div>
+    <div class="bg-amber-200 m-2 p-3 inline-block">
+      Welcome to AIE India
+    </div>
+
+    <div class="bg-amber-200 m-2 p-3 inline-block">
+      Welcome to AIE Hyd
+    </div>
+  </div>
+
+`
+
+setupCounter(document.querySelector('#counter'))
